c0fdd5e868b059e1283bc40bb665a5144c35eaf1
r567tw <r567tw@gmail.com>
2022-01-04 00:05:47 +0800

    Creating an Event to Notify Users’ Session Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
         </nav>
 
         <main class="py-4">
-            <div id="notification" class="alert alert-success mx-3 invisable"></div>
+            <div id="notification" class="alert alert-success mx-3 invisible"></div>
             @yield('content')
         </main>
     </div>
